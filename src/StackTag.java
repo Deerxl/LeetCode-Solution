8d36eb2acaeefe3530ff366ae039b88e907dfddd
xl Deer <YAOXL819@outlook.com>
2020-02-20 17:38:01 +0800

    update

--- src/StackTag.java
+++ src/StackTag.java
@@ -1,12 +1,11 @@
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class StackTag {
 
     /**
      * 求 三种括号是否对称 -easy
-     * @param s
-     * @return
      */
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
@@ -26,8 +25,6 @@ public class StackTag {
 
     /**
      * 接雨水  -hard   栈：遇到上升的向前求面积
-     * @param height
-     * @return
      */
     public int trap(int[] height) {
         int volumn = 0;
@@ -48,8 +45,6 @@ public class StackTag {
 
     /**
      * 简化路径     -medium 栈
-     * @param path
-     * @return
      */
     public String simplifyPath(String path) {
         Stack<String> stack = new Stack<>();
@@ -80,8 +75,6 @@ public class StackTag {
 
     /**
      * 求数组的最大长方体    -hard   栈 / 分治（最低的一长条面积 和左右的最大面积相比）
-     * @param heights
-     * @return
      */
     public int largestRectangleArea(int[] heights) {
         return singleSideArea(heights, 0, heights.length - 1);
@@ -105,8 +98,6 @@ public class StackTag {
 
     /**
      * 求数组的最大长方体 -hard 栈：当高度下降时来依次计算前面的面积
-     * @param heights
-     * @return
      */
     public int largestRectangleArea2(int[] heights) {
         Stack<Integer> stack = new Stack<>();
@@ -127,8 +118,6 @@ public class StackTag {
 
     /**
      * 求二维二进制数组的“1”构成的最大长方形面积   -hard 每行向上累加，以行为单位求最大面积
-     * @param matrix
-     * @return
      */
     public int maximalRectangle(String[][] matrix) {
         int[][] matrix2 = new int[matrix.length][];
@@ -164,5 +153,300 @@ public class StackTag {
         return maxArea;
     }
 
+    /**
+     * 逆波兰表达式求值 -medium 栈存储数值，遇到符号 出栈2个 求值后入栈
+     */
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        int num1, num2;
+        for (int i = 0; i < tokens.length; i++) {
+            switch (tokens[i]) {
+                case "+":
+                    num1 = stack.pop();
+                    num2 = stack.pop();
+                    stack.push(num1 + num2);
+                    break;
+                case "-":
+                    num1 = stack.pop();
+                    num2 = stack.pop();
+                    stack.push(num2 - num1);
+                    break;
+                case "*":
+                    num1 = stack.pop();
+                    num2 = stack.pop();
+                    stack.push(num2 * num1);
+                    break;
+                case "/":
+                    num1 = stack.pop();
+                    num2 = stack.pop();
+                    stack.push(num2 / num1);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(tokens[i]));
+                    break;
+            }
+        }
+        return stack.pop();
+    }
 
+    /**
+     * 计算简单的表达式 -hard   用栈来存储，更简单的方法是用一个栈存储，大于10的数字可以用 x10再 +来解决，减少冗余，更加快
+     */
+    public int calculate(String s) {
+        Stack<Integer> nums = new Stack<>();
+        Stack<Character> ops = new Stack<>();
+        int num1, num2;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '(':
+                    ops.push('(');
+                    break;
+                case '+':
+                case '-':
+                    if (!ops.empty() && (ops.peek() == '-' || ops.peek() == '+')) {
+                        num1 = nums.pop();
+                        num2 = nums.pop();
+                        if (ops.pop() == '+') nums.push(num2 + num1);
+                        else nums.push(num2 - num1);
+                    }
+                    ops.push(c);
+                    break;
+                case ')':
+                    while (ops.peek() != '(') {
+                        num1 = nums.pop();
+                        num2 = nums.pop();
+                        if (ops.pop() == '+') nums.push(num2 + num1);
+                        else nums.push(num2 - num1);
+                    }
+                    ops.pop();  // pop '('
+                    break;
+                case ' ':
+                    break;
+                default:
+                    StringBuffer buffer = new StringBuffer();
+                    buffer.append(String.valueOf(c));
+                    while (i < s.length() - 1 && s.charAt(i + 1) >= '0' && s.charAt(i + 1) <= '9') {
+                        buffer.append(String.valueOf(s.charAt(++i)));
+                    }
+                    nums.push(Integer.parseInt(buffer.toString()));
+                    break;
+            }
+        }
+        if (!ops.empty()) {
+            num1 = nums.pop();
+            num2 = nums.pop();
+            if (ops.pop() == '+') nums.push(num2 + num1);
+            else nums.push(num2 - num1);
+        }
+        return nums.pop();
+    }
 }
+
+/**
+ * 最小栈，-easy 用两个栈，一个栈正常出入，一个栈栈顶存储最小值。
+ */
+class MinStack {
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
+    /** initialize your data structure here. */
+    public MinStack() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        s1.push(x);
+        if (s2.empty() || x <= s2.peek()) {
+            s2.push(x);
+        }
+    }
+
+    public void pop() {
+        if (!s1.empty() && s1.peek().equals(s2.peek())) {
+            s2.pop();
+            s1.pop();
+        } else if (!s1.empty()) {
+            s1.pop();
+        }
+    }
+
+    public int top() {
+        if (!s1.empty()) {
+            return s1.peek();
+        } else {
+            throw new RuntimeException("error: stack is empty.");
+        }
+    }
+
+    public int getMin() {
+        if (!s2.empty()) {
+            return s2.peek();
+        } else {
+            throw new RuntimeException("error: stack is empty.");
+        }
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+
+/**
+ * 二叉搜索树迭代器 -medium 求 next和 hasNext，中序遍历，栈来存储
+ */
+class BSTIterator {
+    Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        leftMostNode(root);
+    }
+
+    private void leftMostNode(TreeNode root) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode root = stack.pop();
+        if (root.right != null) {
+            leftMostNode(root.right);
+        }
+        return root.val;
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.empty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
+
+
+/**
+ * 用栈实现队列 -easy 用两个栈来存储，push用 s1，pop用s2，如果 s2为空，将 s1倒入 s2中
+ */
+class MyQueue {
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return s1.empty() && s2.empty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+
+
+/**
+ * 用队列实现栈 -easy 两个栈来完成，注意 s2每次操作后清空
+ */
+class MyStack {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        q1.add(x);
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        while (q1.size() != 1) {
+            q2.add(q1.remove());
+        }
+        int res = q1.remove();
+        q1 = q2;
+        Queue<Integer> temp = new LinkedList<>();
+        q2 = temp;
+        return res;
+    }
+
+    /** Get the top element. */
+    public int top() {
+        while (q1.size() != 1) {
+            q2.add(q1.remove());
+        }
+        int res = q1.remove();
+        q2.add(res);
+        q1 = q2;
+        Queue<Integer> temp = new LinkedList<>();
+        q2 = temp;
+        return res;
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return q1.size() == 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
